0382431580060b9cb136f2aaf8379f7d340ab3f3
mike whitfield <whitfieldwork@gmail.com>
2022-11-15 14:05:16 -0600

    Add Application project to solution

---
+++ NWCodeFirstMVC/NWCodeFirstMVC.App/Contracts/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWCodeFirstMVC.App.Contracts
+{
+    internal interface IProductService
+    {
+        JsonResult GetAllProducts();
+    }
+}
