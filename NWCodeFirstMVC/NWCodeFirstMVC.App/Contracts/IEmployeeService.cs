134d4cb3ae388e10535934f88440318908f5872b
mwhitfieldwork <mwhitfieldwork@gmail.com>
2025-03-14 16:00:28 -0500

    Add Employee get method to api

---
+++ NWCodeFirstMVC/NWCodeFirstMVC.App/Contracts/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using NWCodeFirstMVC.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWCodeFirstMVC.App.Contracts
+{
+    public interface IEmployeeService: IGenericRepository<Employee>
+    {
+    }
+}
