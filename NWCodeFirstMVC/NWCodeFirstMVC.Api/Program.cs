c1d275f7d89c1679e1d9bac87304a0973bed4653
mwhitfieldwork <mwhitfieldwork@gmail.com>
2025-11-07 17:52:02 -0600

    test branch

--- NWCodeFirstMVC/NWCodeFirstMVC.Api/Program.cs
+++ NWCodeFirstMVC/NWCodeFirstMVC.Api/Program.cs
@@ -28,14 +28,6 @@ builder.Services.AddCors(options =>
         .AllowAnyOrigin()
         .AllowAnyMethod());
 });
-
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(5003);
-    options.ListenAnyIP(7216, listenOptions => listenOptions.UseHttps());
-});
-
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddScoped<ILoginService, LoginService>();
 builder.Services.AddScoped<IOrderHistory, OrderHistoryService>();
@@ -53,10 +45,12 @@ NLog.LogManager.Setup().LoadConfiguration(builder => {
 
 var app = builder.Build();
 
-
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-
+}
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
